48df31383bbf3b7087f25949953274690350152a
Felipe Carrillo <anfelcaro@hotmail.com>
2024-03-31 06:21:25 -0500

    Adicion de imagen en mediumBLOB

--- scripts/dev/2/rollback/1 - entidades.sql
+++ scripts/dev/2/rollback/1 - entidades.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS tbQueue;
 -- Eliminar si existe tabla de imagenes publicitarias almacenadas en bits tbImages
 DROP TABLE IF EXISTS tbImages;
 
--- Eliminar si existe tabla para relacionar tbimagenes con un grupo de imagenes tbImageGroup
-DROP TABLE IF EXISTS tbImageGroup;
-
 -- Eliminar si existe tabla para relacionar muchos a muchos entre usuarios y grupos de imagenes tbUserImageGroupRelation
 DROP TABLE IF EXISTS tbUserImageGroupRelation;
+
+-- Eliminar si existe tabla para relacionar tbimagenes con un grupo de imagenes tbImageGroup
+DROP TABLE IF EXISTS tbImageGroup;
